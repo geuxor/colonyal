8b643d50fee8c0198969c7a8476e8d0ccca9147e
German B <geuxor@dr.com>
2021-07-30 19:46:09 +0200

    client-login auth creation

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,10 +1,23 @@
+// require('dotenv').config()
 import axios from 'axios'
+console.log('welcome to actions/auth.js');
+console.log(process.env.REACT_APP_API)
 
 export const registerUser = async (user) =>
   await axios.post(`${process.env.REACT_APP_API}/register`, user);
 
-export const loginUser = async (user) =>
-  await axios.post(`${process.env.REACT_APP_API}/login`, user);
+async function loginUser (user) {
+  console.log('ready to send', user);
+  
+  await axios.post(`${process.env.REACT_APP_API}/login`, user, () => {
+    console.log(user);
+  });
+}
+
+
+// export const loginUser = async (user) =>
+//   await axios.get(`${process.env.REACT_APP_API}/login`, user, { withCredentials: true });
+
 
 // update user in local storage / next() call back to redirect user to dashboard 
 export const updateUserInLocalStorage = (user, next) => {
@@ -15,3 +28,5 @@ export const updateUserInLocalStorage = (user, next) => {
     next();
   }
 };
+
+export { loginUser }
